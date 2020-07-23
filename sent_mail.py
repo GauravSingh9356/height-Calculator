a7a2cea0fd290253e43f9c274d2ab591331fd8a9
GauravSingh75 <55052983+GauravSingh9356@users.noreply.github.com>
2020-07-23 18:32:11 +0530

    ceredentials protected

--- sent_mail.py
+++ sent_mail.py
@@ -1,8 +1,9 @@
 from email.mime.text import MIMEText
 import smtplib
+
 def sent_email(email, height, avg_height, count):
-    from_email = 'gs935688@gmail.com'
-    from_password='spectrum12345##'
+    from_email = process.env.email
+    from_password=process.env.password
     to_email=email
     subject='Height data'
     message='Hey there, your height is <strong>%s</strong>.<br/> Average height of all is <strong>%s</strong>. <br/><strong>%s</strong> no of people are recorded.<br/>Thanks!' % (height, avg_height, count)
